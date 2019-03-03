7d94a367cf9ea84d8bb8a17c2465e2812f659a91
Ajay Singh <39572729+KappaTrooper@users.noreply.github.com>
2019-03-03 15:38:36 -0500

    Add files via upload

---
+++ js/selection.js
@@ -0,0 +1,60 @@
+document.getElementsByClassName("madara-audio").forEach(el => {el.pause()});
+document.getElementsByClassName("naruto-audio").forEach(el => {el.pause()});
+document.getElementsByClassName("mikasa-audio").forEach(el => {el.pause()});
+document.getElementsByClassName("erza-audio").forEach(el => {el.pause()});
+document.getElementsByClassName("erza-audio").forEach(el => {el.pause()});
+document.getElementsByClassName("madara-audio").forEach(el => {el.pause()});
+document.getElementsByClassName("madara-audio").forEach(el => {el.pause()});
+document.getElementsByClassName("madara-audio").forEach(el => {el.pause()});
+
+
+function Madara() {
+  document.getElementById("character-box").style.backgroundImage = "url(Characters/Madaraeyes.gif)";
+   var audio = document.getElementById("madara-audio");
+   audio.play();
+}
+
+function Naruto() {
+  document.getElementById("character-box").style.backgroundImage = "url(Characters/narutolaugh.gif)";
+    var audio = document.getElementById("naruto-audio");
+    audio.play();
+}
+
+function mikasa() {
+  document.getElementById("character-box").style.backgroundImage = "url(Characters/mikasaeye.gif)";
+    var audio = document.getElementById("mikasa-audio");
+    audio.play();
+}
+
+function erza() {
+  document.getElementById("character-box").style.backgroundImage = "url(Characters/ezrawalk.gif)";
+    var audio = document.getElementById("erza-audio");
+    audio.play();
+}
+
+function gray() {
+  document.getElementById("character-box").style.backgroundImage = "url(Characters/gray.gif)";
+    var audio = document.getElementById("erza-audio");
+    audio.play();
+}
+
+function ichigo() {
+  document.getElementById("character-box").style.backgroundImage = "url(Characters/ichigo.gif)";
+    var audio = document.getElementById("ichigo-audio");
+    audio.play();
+}
+
+function fdf() {
+  document.getElementById("character-box").style.backgroundImage = "url(Characters/Naruto.png)";
+    var audio = document.getElementById("naruto-audio");
+    audio.play();
+}
+
+function hh() {
+  document.getElementById("character-box").style.backgroundImage = "url(Characters/Naruto.png)";
+    var audio = document.getElementById("naruto-audio");
+    audio.play();
+}
+
+document.getElementById("bckmusic").volume = 0.1;
+
